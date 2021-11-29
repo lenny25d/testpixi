e381bc5cb2b1e53674efac461f45c6d8cb524c82
Lenny <lenny@editmate.kr>
2021-11-29 11:53:53 +0900

    init

---
+++ src/vdom/dom-api.ts
@@ -0,0 +1,71 @@
+import { VNodeType, ICustomAPI, Is, flatten1, setComponent, getComponent,
+} from './'
+import * as Hydux from 'hydux'
+
+export const domApi: ICustomAPI<Node> = {
+  createElement(node): Node {
+    if (!Is.def(node)) {
+      return document.createElement('')
+    }
+    switch (node.type) {
+      case VNodeType.text:
+        return document.createTextNode(node.name)
+      case VNodeType.element:
+        const el = document.createElement(node.name)
+        return el
+      default:
+        return Hydux.never(node)
+    }
+  },
+  setAttribute(node, key, val, attrs) {
+    if (Is.fn(val) || !Is.def(val)) {
+      // ignore
+    } else if (val == null || val === false) {
+      (node as HTMLElement).removeAttribute(key)
+    } else if (key === 'style') {
+      Object.assign((node as HTMLElement).style, val)
+    } else if (
+      (key in node || (
+        key[0] === 'o' &&
+        key[1] === 'n'
+      )) &&
+      key !== 'list' &&
+      !('ownerSVGElement' in node)
+    ) {
+      node[key] = val == null ? '' : val
+    } else {
+      (node as HTMLElement).setAttribute(key, val)
+    }
+  },
+  insertAt(parentNode, newNode, i) {
+    let len = parentNode.childNodes.length
+    if (i < parentNode.childNodes.length) {
+      const node = parentNode.childNodes[i]
+      parentNode.insertBefore(newNode, node)
+    } else {
+      parentNode.appendChild(newNode)
+    }
+    return parentNode.childNodes.length - len
+  },
+  replaceChild(parentNode, newNode, node) {
+    parentNode.replaceChild(newNode, node)
+  },
+  getChildAt(node, i) {
+    return node.childNodes[i]
+  },
+  getChildCount(node) {
+    return node.childNodes.length
+  },
+  getParent(node) {
+    return node.parentNode
+  },
+  removeChildAt(node, i) {
+    let child = node.childNodes[i]
+    return node.removeChild(child)
+  },
+  setTextContent(node, text) {
+    (node as Text).data = text
+  }
+}
+
+export default domApi
